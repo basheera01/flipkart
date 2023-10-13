cf54f61996eae2a60762542cb3320e332059575e
basheera01 <shaikbasheeraa@gmail.com>
2023-10-13 15:14:47 +0530

    added products

--- src/components/Form.js
+++ src/components/Form.js
@@ -66,7 +66,7 @@ const handleEdit=(ed)=>{
 }
    console.log(products)
    const handleDelete=(id)=>{
-     axios.delete(`http://localhost:3002/products/${id}`,productData)
+     axios.delete(`http://localhost:3002/products/${id}`)
      .then((res)=>{
          alert('Product Deleted');
          fetchData()
